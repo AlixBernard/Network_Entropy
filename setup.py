9e328ced3c64a4e65cda88aac0f17ee8668df645
AlixBernard <alix.bernard9@gmail.com>
2022-10-06 01:19:30 +0700

    Bumped up version minor

--- setup.py
+++ setup.py
@@ -2,11 +2,18 @@
 
 from distutils.core import setup
 
-setup(name='network_entropy',
-      version='0.2',
-      #description='',
-      author='Alix Bernard',
-      author_email='alix.bernard9@gmail.com',
-      url='https://github.com/AlixBernard/Network_Entropy',
-      packages=['network_entropy'],
-     )
+
+MAJOR = 0
+MINOR = 3
+PATCH = 0
+
+
+setup(
+    name="network_entropy",
+    version=f"{MAJOR}.{MINOR}.{PATCH}",
+    description="Library to compute the entropy of a network.",
+    author="Alix Bernard",
+    author_email="alix.bernard9@gmail.com",
+    url="https://github.com/AlixBernard/Network_Entropy",
+    packages=["network_entropy"],
+)
